a12e1f0e47f830fe5ac99f1db82cfeb59a1d9fed
susan <susan@users.noreply.github.com>
2019-04-21 12:20:34 -0400

    getting back the cart items as book obj

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,9 +4,11 @@ class Api::V1::AuthController <ApplicationController
 	def create
       @user = User.find_by(email: login_user_params[:email])
       @cart = Cart.find_by(:user_id => @user.id)
+      @cart_items = cart_items(@cart)
+      #byebug
       if @user && @user.authenticate(login_user_params[:password])
         token = encode_token({ user_id: @user.id })
-        render json: { user: @user, cart: @cart, jwt: token }, status: :accepted
+        render json: { user: @user, cart_items: @cart_items, jwt: token }, status: :accepted
       else
         render json: { message: 'Invalid email or password' }, status: :unauthorized
       end
@@ -21,6 +23,16 @@ class Api::V1::AuthController <ApplicationController
         render json: { message: 'Invalid email or password' }, status: :unauthorized
       end
     end
+
+ def cart_items(cart)
+    @items = LineItem.all.select do |item|
+        item.cart_id === cart.id
+    end
+    @books = @items.map do |item|
+      item.book
+    end
+    @books
+ end
 
     private
 
