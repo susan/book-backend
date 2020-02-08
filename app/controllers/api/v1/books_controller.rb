071b14e7c1fbca73662173719a29dfadd4d429ba
susan <susan@users.noreply.github.com>
2020-02-08 18:33:06 -0500

    update quantity to add only

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,24 +8,27 @@ class Api::V1::BooksController < ApplicationController
 
 
   def create
-  	if curr_user
-  		#byebug
-      @book = Book.find_by(:title => params[:book][:title])
-        if !@book
-         @book = Book.create(book_params)
-        end
-      @line_item = create_line_item(@book, curr_user)
+  	if curr_user && @book = Book.find_by(title: params[:book][:title])
+      if !@book
+        @book = Book.create(book_params)
+      end
+      @line_item = curr_user.cart.line_items.find_by(book_id: @book.id)
+      if @line_item
+        @line_item.quantity+=1
+      else
+       @line_item = create_line_item(@book, curr_user)
+      end 
       if @line_item.save
         render json: {book: @book, line_item: @line_item }, status: :created
       else
      	  render json: { errors: @line_item.errors.full_messages }, status: :unprocessible_entity
       end
-   end
+    end 
   end
 
    def create_line_item(book, user)
-    @cart = Cart.find_by(:user_id => user.id)
-    @line_item = LineItem.create(book_id: book.id, cart_id: @cart.id)
+    @cart = Cart.find_by(user_id: user.id)
+    line_item = LineItem.create(book_id: book.id, cart_id: @cart.id, quantity:1)
    end
 
 private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
-  belongs_to :user
 	has_many :line_items
   has_many :carts, through: :line_items, dependent: :destroy
   validates_uniqueness_of :title
